78aac27f23ac56fb245a47ddf6a5832001d7ce1f
RanjithKumar <ranjithkmr598@gmail.com>
2020-09-29 22:41:29 +0530

    pre Table data

--- affiliateLinks.py
+++ affiliateLinks.py
@@ -1,10 +1,39 @@
 from selenium import webdriver
 import time
+import pandas as pd
 
 browser = webdriver.Chrome()
 
 link1 = 'https://affiliate-program.amazon.com/home'
-link2 = 'https://www.amazon.com/gp/product/B01HK0W01E?pf_rd_r=P0CXCD91483KJ3R4HRKB&pf_rd_p=edaba0ee-c2fe-4124-9f5d-b31d6b1bfbee'
+
+
+def getAllData(link2):
+    lis2 = []
+    browser.get(link2)
+    time.sleep(3)
+    #      ASSOCIATE URL
+    text_box = browser.find_element_by_id('amzn-ss-text-link')
+    text_box.click()
+    time.sleep(2)
+    text_full_link = browser.find_element_by_id('amzn-ss-full-link-radio-button')
+    text_full_link.click()
+    time.sleep(2)
+    text_url = browser.find_element_by_id('amzn-ss-text-fulllink-textarea')
+    lis2.append(text_url.text)
+
+    #      ASSOCIATE IMAGE URL
+    image_box = browser.find_element_by_id('amzn-ss-image-link')
+    image_box.click()
+    time.sleep(2)
+    image_url = browser.find_element_by_id('amzn-ss-image-textarea')
+    lis2.append(image_url.text)
+
+    #       PRODUCT TITLE
+    product_title = browser.find_element_by_id('productTitle')
+    lis2.append(product_title.text)
+
+    return lis2
+
 
 # First Part Associate Sign in
 browser.get(link1)
@@ -21,17 +50,21 @@ time.sleep(5)
 
 browser.execute_script("window.open('');")
 browser.switch_to.window(browser.window_handles[1])
-browser.get(link2)
-time.sleep(5)
 
-text = browser.find_element_by_id('amzn-ss-text-link')
-text.click()
+#       READING THE LINKS FROM EXCEL SHEET
+links_df = pd.read_csv("links.csv")
+lis = []
+lis3 = []
 
-time.sleep(4)
-text2 = browser.find_element_by_id('amzn-ss-full-link-radio-button')
-text2.click()
 
-time.sleep(2)
-text3 = browser.find_element_by_id('amzn-ss-text-fulllink-textarea')
-text_3 = text3.text
-print(text_3)
+for index, row in links_df.iterrows():
+    lis.append(row.links)
+
+#       ITERATING THROUGH EACH LINK
+
+for link in lis:
+    lis3.append(getAllData(link))
+
+links_df2 = pd.DataFrame(lis3, columns=['url','image','title'])
+
+links_df2.to_csv('output2.csv')

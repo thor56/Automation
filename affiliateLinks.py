4e56f6b78d888d528088005674ac9a285fc2bd48
RanjithKumar <ranjithkmr598@gmail.com>
2020-10-01 19:58:55 +0530

    Generating the Inputs for the Table

--- affiliateLinks.py
+++ affiliateLinks.py
@@ -1,30 +1,30 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import time
 import pandas as pd
 
-browser = webdriver.Chrome()
+# browser = webdriver.Chrome()
 
 link1 = 'https://affiliate-program.amazon.com/home'
-
 
 def getAllData(link2):
     lis2 = []
     browser.get(link2)
-    time.sleep(3)
+    time.sleep(2)
     #      ASSOCIATE URL
     text_box = browser.find_element_by_id('amzn-ss-text-link')
     text_box.click()
-    time.sleep(2)
+    time.sleep(1)
     text_full_link = browser.find_element_by_id('amzn-ss-full-link-radio-button')
     text_full_link.click()
-    time.sleep(2)
+    time.sleep(1)
     text_url = browser.find_element_by_id('amzn-ss-text-fulllink-textarea')
     lis2.append(text_url.text)
 
     #      ASSOCIATE IMAGE URL
     image_box = browser.find_element_by_id('amzn-ss-image-link')
     image_box.click()
-    time.sleep(2)
+    time.sleep(1)
     image_url = browser.find_element_by_id('amzn-ss-image-textarea')
     lis2.append(image_url.text)
 
@@ -33,33 +33,45 @@ def getAllData(link2):
     lis2.append(product_title.text)
 
     #       PRODUCT DESCRIPTION
-    product_desc = browser.find_element_by_class_name('a-unordered-list a-vertical a-spacing-mini')
-    lis2.append(product_desc)
+    html_list = browser.find_element_by_id("feature-bullets")
+    items = html_list.find_elements_by_tag_name("li")
+    desc_bullets = []
+    for item in items:
+        text = item.text
+        desc_bullets.append(text)
+    lis2.append(desc_bullets)
 
     return lis2
 
 
-# First Part Associate Sign in
-browser.get(link1)
-u_name = browser.find_element_by_id('ap_email')
-u_name.send_keys('tcfutube@gmail.com')
-pass_wd = browser.find_element_by_id('ap_password')
-pass_wd.send_keys('9441813435@rk')
-button = browser.find_element_by_id('signInSubmit')
+chrome_options = Options()
+chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
 
-button.click()
-time.sleep(5)
+# Change chrome driver path accordingly
+chrome_driver = "E:\PROJECTS\Automation\chromedriver.exe"
+browser = webdriver.Chrome(chrome_driver, options=chrome_options)
+
+# # First Part Associate Sign in
+# browser.get(link1)
+# # u_name = browser.find_element_by_id('ap_email')
+# # u_name.send_keys('tcfutube@gmail.com')
+# # pass_wd = browser.find_element_by_id('ap_password')
+# # pass_wd.send_keys('9441813435@rk')
+# # button = browser.find_element_by_id('signInSubmit')
+# #
+# # button.click()
+# # time.sleep(5)
 
 # Second part  --- Scrapping
 
-browser.execute_script("window.open('');")
-browser.switch_to.window(browser.window_handles[1])
+# browser.execute_script("window.open('');")
+# browser.switch_to.window(browser.window_handles[1])
 
 #       READING THE LINKS FROM EXCEL SHEET
+
 links_df = pd.read_csv("links.csv")
 lis = []
 lis3 = []
-
 
 for index, row in links_df.iterrows():
     lis.append(row.links)
@@ -71,4 +83,4 @@ for link in lis:
 
 links_df2 = pd.DataFrame(lis3, columns=['url','image','title','description'])
 
-links_df2.to_csv('output2.csv')
+links_df2.to_csv('TableInput.csv')

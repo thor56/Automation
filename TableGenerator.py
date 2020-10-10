11e64ad307230e4d4b7320fd0ad9842e2730bafa
Ranjith Kumar <ranithkmr598@gmail.com>
2020-10-11 01:15:13 +0530

    Everything works

--- TableGenerator.py
+++ TableGenerator.py
@@ -2,33 +2,44 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
 import pandas as pd
+import re
 
-link = "https://app.affiliatable.io/posts"
 
-def CreateTable(postTitle):
-    pass
+link = "https://startblogging.co/affiliate-product-table-generator"
 
-def InsertRow(title,url,image,description):
-    pass
+def InsertRow(title,url,image,description,count):
 
+        # add product
+    browser.find_element_by_xpath('/html/body/div/div/div/div[1]/main/div/div/div[1]/div/div/div[2]/button').click()
+    # title
+    browser.find_element_by_xpath('/html/body/div/div/div/div/main/div/div/div[1]/div/div/div[2]/div[1]/div[' + str(count + 1) + ']/div/div[1]/div[1]/div/div[1]/div/input').send_keys(title)
+    # image
+    image1 = re.search('src="(.*)" ></a>', image).group(1)
+    browser.find_element_by_xpath('/html/body/div/div/div/div/main/div/div/div[1]/div/div/div[2]/div[1]/div[' + str(count + 1) + ']/div/div[2]/div[1]/div/div[1]/div/input').send_keys(image1)
+    # url
+    browser.find_element_by_xpath('/html/body/div/div/div/div/main/div/div/div[1]/div/div/div[2]/div[1]/div[' + str(count + 1) + ']/div/div[2]/div[2]/div/div[1]/div/input').send_keys(url)
+    # description
+    browser.find_element_by_xpath('/html/body/div/div/div/div/main/div/div/div[1]/div/div/div[2]/div[1]/div[' + str(count + 1) + ']/div/div[1]/div[3]/div/div[1]/div/textarea').send_keys(description)
 
 # Using an existing Chrome session
-chrome_options = Options()
-chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+# chrome_options = Options()
+# chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
 
 #       Change chrome driver path accordingly
-chrome_driver = "D:\PROJECT\Automation\chromedriver.exe"
-browser = webdriver.Chrome(chrome_driver, options=chrome_options)
+# chrome_driver = "D:\PROJECT\Automation\chromedriver.exe"
+# browser = webdriver.Chrome(chrome_driver, options=chrome_options)
+firefox_Driver = "D:\PROJECT\Automation\geckodriver-v0.27.0-win64"
+browser = webdriver.Firefox(firefox_Driver)
 
-metadata_df = pd.read_csv("TableDetails.csv")
+
 data_df = pd.read_csv("TableInput.csv")
 
-# getting table metadata
-for index, row in metadata_df.iterrows():
-    CreateTable(row.postTitle)
+browser.get(link)
 
 # Looping and creating table rows
+count = 0
 for index, row in data_df.iterrows():
-    InsertRow(row.title,row.url,row.image,row.description)
+    InsertRow(row.title,row.url,row.image,row.description,count)
+    count = count + 1
+    time.sleep(2)
 
-#  Saving the table output to clipboard
